5a636331e3e470f03f5048879401e2ec6db02b46
unsteelix <unsteelix@yandex.ru>
2025-02-04 14:13:56 +0000

    init

---
+++ gitadd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+sudo git add cloudflare/docker-compose.yml
+sudo git add filebrowser/docker-compose.yml
+sudo git add homepage/docker-compose.yml
+sudo git add jellyfin/docker-compose.yml
+sudo git add jellyseerr/docker-compose.yml
+sudo git add memos/docker-compose.yml
+sudo git add nginx/docker-compose.yml
+sudo git add pihole/docker-compose.yml
+sudo git add portfolio/docker-compose.yml
+sudo git add prowlarr/docker-compose.yml
+sudo git add qbittorrent/docker-compose.yml
+sudo git add radarr/docker-compose.yml
+sudo git add samba/docker-compose.yml
+
+sudo git add gitadd.sh
+
+echo "success"
