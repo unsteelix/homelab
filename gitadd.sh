26c727ce509da127abedd4eeef8692a5834a467e
unsteelix <unsteelix@yandex.ru>
2025-02-08 08:30:14 +0000

    some refactor

--- gitadd.sh
+++ gitadd.sh
@@ -1,18 +1,11 @@
 #!/bin/bash -e
 
-sudo git add cloudflare/docker-compose.yml
-sudo git add filebrowser/docker-compose.yml
-sudo git add homepage/docker-compose.yml
-sudo git add jellyfin/docker-compose.yml
-sudo git add jellyseerr/docker-compose.yml
-sudo git add memos/docker-compose.yml
-sudo git add nginx/docker-compose.yml
-sudo git add pihole/docker-compose.yml
-sudo git add prowlarr/docker-compose.yml
-sudo git add qbittorrent/docker-compose.yml
-sudo git add radarr/docker-compose.yml
-sudo git add samba/docker-compose.yml
+for folderName in $(ls -F)
+do
+    if [[ "$folderName" == *"/"* ]]; then
+        git add "$folderName"docker-compose.yml
+    fi
+    
+done
 
 sudo git add gitadd.sh
-
-echo "success"
